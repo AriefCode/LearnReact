9463dd268c9e0189f8ce283f0f4ba701457014d7
M. Arief <90082998+AriefCode@users.noreply.github.com>
2026-03-11 23:43:09 +0700

    Finish modul pertemuan 2

---
+++ src/pertemuan-1/HelloWorld.jsx
@@ -0,0 +1,63 @@
+export default function HelloWorld() {
+  const propsUserCard = {
+    nama: "Goku",
+    nim: "999999",
+    tanggal: "2025-01-01",
+  };
+  return (
+    <div>
+      <h1>Hello World</h1>
+      <p>Selamat belajar ReactJS</p>
+      <GreetingDuri />
+      <QuoteText />
+      <UserCard
+        // CARA GANTENG
+        {...propsUserCard}
+
+        // CARA GA GANTENG
+        // nama={propsUserCard.nama}
+        // nim={propsUserCard.nim}
+        // tanggal={propsUserCard.tanggal}
+
+        // CARA BIASA
+        // nama= "Arief"
+        // nim="2455301093"
+        // tanggal={new Date().toLocaleDateString()}
+      />
+
+      <img src="img/wondie.jpg" alt="wondie cantiks" />
+    </div>
+  );
+}
+
+function GreetingDuri() {
+  return (
+    <div>
+      <small>Salam dari Duri</small>
+      <p>ijin</p>
+    </div>
+  );
+}
+
+function QuoteText() {
+  const text = "Mulutmu Harimaumu";
+  const text2 = "Aku ingin jadi macan";
+  return (
+    <div>
+      <hr />
+      <p>{text.toLowerCase()}</p>
+      <p>{text2.toUpperCase()}</p>
+    </div>
+  );
+}
+
+function UserCard(props) {
+  return (
+    <div>
+      <hr />
+      <h3>Nama: {props.nama}</h3>
+      <h3>NIM: {props.nim}</h3>
+      <h3>Tanggal: {props.tanggal}</h3>
+    </div>
+  );
+}
